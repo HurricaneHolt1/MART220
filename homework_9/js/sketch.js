a1d66f443eccf158d74d404b12f2f7c5d3e1e0ed
HurricaneHolt1 <billballbaggins1929@gmail.com>
2026-03-28 10:51:49 -0600

    Update sketch.js

--- homework_9/js/sketch.js
+++ homework_9/js/sketch.js
@@ -9,19 +9,20 @@ function setup() {
 function draw() {
   background(220);
 
-  if (kb.pressing("left"))  player.x -= 4;
-  if (kb.pressing("right")) player.x += 4;
-  if (kb.pressing("up"))    player.y -= 4;
-  if (kb.pressing("down"))  player.y += 4;
+  // Use plain p5.js keyIsDown instead of kb.pressing
+  if (keyIsDown(LEFT_ARROW)  || keyIsDown(65))  player.x -= 4; // 65 = A
+  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68))  player.x += 4; // 68 = D
+  if (keyIsDown(UP_ARROW)    || keyIsDown(87))  player.y -= 4; // 87 = W
+  if (keyIsDown(DOWN_ARROW)  || keyIsDown(83))  player.y += 4; // 83 = S
 
   fill(0);
   noStroke();
   textSize(16);
   textAlign(LEFT);
-  text("left:  " + kb.pressing("left"),  20, 30);
-  text("right: " + kb.pressing("right"), 20, 50);
-  text("up:    " + kb.pressing("up"),    20, 70);
-  text("down:  " + kb.pressing("down"),  20, 90);
+  text("keyIsDown LEFT:  " + keyIsDown(LEFT_ARROW),  20, 30);
+  text("keyIsDown RIGHT: " + keyIsDown(RIGHT_ARROW), 20, 50);
+  text("keyIsDown UP:    " + keyIsDown(UP_ARROW),    20, 70);
+  text("keyIsDown DOWN:  " + keyIsDown(DOWN_ARROW),  20, 90);
   text("x: " + round(player.x) + "  y: " + round(player.y), 20, 110);
-  text("Click the canvas first, then use arrow keys or WASD", 20, 140);
+  text("Click canvas then press arrow keys or WASD", 20, 140);
 }
