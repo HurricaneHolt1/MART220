1eaafbf442f8d80b2fbb007adc93d3823bb81481
HurricaneHolt1 <billballbaggins1929@gmail.com>
2026-03-28 10:45:53 -0600

    Update sketch.js

--- homework_9/js/sketch.js
+++ homework_9/js/sketch.js
@@ -1,27 +1,19 @@
-// MINIMAL DEBUG SKETCH
-// Tests: 1) does the sprite move with vel.x, 2) does kb.pressing work
-
 let player;
 
 function setup() {
   new Canvas(800, 600);
-  player = new Sprite(400, 300, 40, 40);
+  player = new Sprite(400, 300, 40, 40, "none");
   player.color = "red";
-  player.rotationLock = true;
-  player.drag = 0;
-  player.friction = 0;
 }
 
 function draw() {
   background(220);
 
-  // try direct position nudge as a fallback
-  if (kb.pressing("left"))  { player.x -= 4; }
-  if (kb.pressing("right")) { player.x += 4; }
-  if (kb.pressing("up"))    { player.y -= 4; }
-  if (kb.pressing("down"))  { player.y += 4; }
+  if (kb.pressing("left"))  player.x -= 4;
+  if (kb.pressing("right")) player.x += 4;
+  if (kb.pressing("up"))    player.y -= 4;
+  if (kb.pressing("down"))  player.y += 4;
 
-  // show kb state on screen
   fill(0);
   noStroke();
   textSize(16);
@@ -30,7 +22,6 @@ function draw() {
   text("right: " + kb.pressing("right"), 20, 50);
   text("up:    " + kb.pressing("up"),    20, 70);
   text("down:  " + kb.pressing("down"),  20, 90);
-  text("player x: " + player.x, 20, 110);
-  text("player y: " + player.y, 20, 130);
-  text("Press arrow keys or WASD", 20, 160);
+  text("x: " + round(player.x) + "  y: " + round(player.y), 20, 110);
+  text("Click the canvas first, then use arrow keys or WASD", 20, 140);
 }
