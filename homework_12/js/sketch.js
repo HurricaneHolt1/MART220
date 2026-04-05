d2b54fb6a5bb7dc2dccf90aed9f510efdd92b590
HurricaneHolt1 <billballbaggins1929@gmail.com>
2026-04-05 16:00:23 -0600

    Update sketch.js

--- homework_12/js/sketch.js
+++ homework_12/js/sketch.js
@@ -6,9 +6,6 @@ let angle5 = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  textFont('Arial');
-  textSize(32);
-  textAlign(CENTER, CENTER);
 }
 
 function draw() {
@@ -16,32 +13,29 @@ function draw() {
 
   // Lighting
   directionalLight(255, 255, 255, 0.25, 0.25, -1);
-  ambientLight(50);
+  ambientLight(100);
 
   // ----- BOX -----
   push();
   translate(-200, -100, 0);
   rotateX(angle1);
   rotateY(angle1 * 0.3);
-  rotateZ(angle1 * 0.2);
   ambientMaterial(200, 50, 50);
-  box(100, 100, 100);
+  box(100);
   pop();
 
   // ----- SPHERE -----
   push();
   translate(150, -150, -100);
   rotateY(angle2);
-  rotateX(angle2 * 0.4);
   specularMaterial(50, 200, 50);
-  sphere(60, 24, 24);
+  sphere(60);
   pop();
 
   // ----- CONE -----
   push();
   translate(-150, 150, 50);
   rotateX(angle3);
-  rotateZ(angle3 * 0.5);
   normalMaterial();
   cone(50, 120);
   pop();
@@ -50,9 +44,8 @@ function draw() {
   push();
   translate(200, 100, 100);
   rotateY(angle4);
-  rotateZ(angle4 * 0.7);
   ambientMaterial(50, 50, 200);
-  cylinder(40, 120, 24, 1);
+  cylinder(40, 120);
   pop();
 
   // ----- TORUS -----
@@ -61,7 +54,7 @@ function draw() {
   rotateX(angle5);
   rotateY(angle5 * 0.5);
   specularMaterial(200, 200, 50);
-  torus(70, 20, 24, 16);
+  torus(70, 20);
   pop();
 
   // Update rotation angles
@@ -70,19 +63,4 @@ function draw() {
   angle3 += 0.015;
   angle4 += 0.018;
   angle5 += 0.013;
-
-  // ----- TEXT -----
-  push();
-  translate(0, -height / 2 + 50, 0);
-  rotateX(-PI / 6); // tilt text to face camera better
-  fill(255);
-  text("3D Exploration", 0, 0);
-  pop();
-
-  push();
-  translate(0, height / 2 - 50, 0);
-  rotateX(-PI / 6);
-  fill(255);
-  text("By Your Name", 0, 0);
-  pop();
 }
